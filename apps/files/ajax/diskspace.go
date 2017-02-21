29efffc435a9033e978606c2b92552150b35f554
Lee Smet <lee.smet@hotmail.com>
2017-02-21 10:58:59 +0100

    Explicit type cast to resolve different type on darwin

--- apps/files/ajax/diskspace.go
+++ apps/files/ajax/diskspace.go
@@ -19,7 +19,7 @@ func getFreeDiskSpace() (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	space := int64(stats.Bavail) * stats.Bsize // unused blocks * the size of one block
+	space := int64(stats.Bavail) * int64(stats.Bsize) // unused blocks * the size of one block
 	log.Debugf("available space: %v bytes", space)
 	return space, nil
 }
